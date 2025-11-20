bddfe84a5789fe93ed4d4b83cc7f7c35a205ca57
DmitriyZhadan <dzadan73@gmail.com>
2025-11-20 19:10:49 +0100

    build: init backend

---
+++ backend/TestApi.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace TestApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
---
+++ backend/TestApi/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers
+{
+    /// <summary>
+    /// Represents an controller that provides endpoints for handling HTTP requests related to test opeations.
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+
+    }
+}
