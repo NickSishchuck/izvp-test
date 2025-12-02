ba7c209c52d2b0e9d8022f3936243c8e2c6c3b47
DmitriyZhadan <dzadan73@gmail.com>
2025-12-02 16:24:52 +0100

    [Backend] feat: add endpoint to check if user passed the test

---
+++ backend/TestApi/UseCases/Handlers/CheckUserPassedQuery.cs
@@ -0,0 +1,39 @@
+﻿using CSharpFunctionalExtensions;
+using FluentValidation.Results;
+using MediatR;
+using TestApi.Interfaces;
+using TestApi.UseCases.Queries;
+
+namespace TestApi.UseCases.Handlers
+{
+    /// <summary>
+    /// Handles the <see cref="CheckUserPassedQuery"/> to verify if a user has already completed a specific test.
+    /// </summary>
+    public class CheckUserPassedQueryHandler(
+        ITestRepository repository,
+        ILogger<CheckUserPassedQueryHandler> logger)
+        : IRequestHandler<CheckUserPassedQuery, Result<Unit, ValidationResult>>
+    {
+        public async Task<Result<Unit, ValidationResult>> Handle(CheckUserPassedQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Checking if user {Username} has already passed test {TestId}", request.Username, request.TestId);
+
+            var userPassed = await repository.UserAlreadyPassedAsync(request.Username, request.TestId, cancellationToken);
+
+            if (userPassed)
+            {
+                logger.LogWarning("User {Username} has already passed test {TestId}", request.Username, request.TestId);
+
+                var validationResult = new ValidationResult(new[]
+                {
+                    new ValidationFailure("Test", "User already passed the test.")
+                });
+
+                return Result.Failure<Unit, ValidationResult>(validationResult);
+            }
+            logger.LogInformation("User {Username} has not passed test {TestId} yet", request.Username, request.TestId);
+
+            return Result.Success<Unit, ValidationResult>(Unit.Value);
+        }
+    }
+}
---
+++ backend/TestApi/UseCases/Queries/CheckUserPassedQuery.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CSharpFunctionalExtensions;
+using FluentValidation.Results;
+using MediatR;
+
+namespace TestApi.UseCases.Queries
+{
+    /// <summary>
+    /// Represents a query to check if a user has already passed a specific test.
+    /// </summary>
+    /// <param name="Username">The username of the user to check.</param>
+    /// <param name="TestId">The unique identifier of the test.</param>
+    [ExcludeFromCodeCoverage]
+    public record CheckUserPassedQuery(string Username, Guid TestId) : IRequest<Result<Unit, ValidationResult>>;
+}
